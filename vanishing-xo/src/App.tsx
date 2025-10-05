22592beabeff986f69b3f967a200b93615913a80
Lusanda Nyilenda <lusandanyilenda@gmail.com>
2025-10-05 20:20:55 +0200

    start project and create grid

---
+++ vanishing-xo/src/App.tsx
@@ -0,0 +1,13 @@
+import Grid from './components/Grid'
+import './App.css'
+
+function App() {
+  
+  return (
+    <div id="root">
+      <Grid/>
+    </div>
+  )
+}
+
+export default App
---
+++ vanishing-xo/src/components/Grid.tsx
@@ -0,0 +1,17 @@
+import "./Grid.css"
+
+export default function Grid() {
+    return (
+        <div className="grid-container">
+            <div id="1">1</div>
+            <div id="2">2</div>
+            <div id="3">3</div>
+            <div id="4">4</div>
+            <div id="5">5</div>
+            <div id="6">6</div>
+            <div id="7">7</div>
+            <div id="8">8</div>
+            <div id="9">9</div>
+        </div>
+    )
+}
